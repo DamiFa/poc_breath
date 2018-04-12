a406830278c6bf31e9d1c14561478d11e8d5eceb
damifa <damifa@debian.dami>
2018-04-12 19:25:20 +0200

    add wobble function, add the mouse speed

--- public/scripts/drawing.js
+++ public/scripts/drawing.js
@@ -1,15 +1,20 @@
 var path = new Path();
+var breathStrength = 1;
 
 var CP = {
-   x: view.center.x,
-   y: view.center.y,
-};
+    x: view.center.x,
+    y: view.center.y,
+ };
+var previousMousePoint;
+var mousePoint;
+var speed = 1;
+
 
 
 function initializePath() {
     center = CP;
 	path.segments = [];
-	for (var i = 1; i < 500; i++) {
+	for (var i = 0; i < 500; i++) {
 
         var tP = new paper.Point({
             length: 2 * i,
@@ -21,7 +26,7 @@ function initializePath() {
         
 		path.add(tP);
 	}
-    path.fullySelected = true;
+    // path.fullySelected = true;
 }
 
 path.style = {
@@ -31,10 +36,32 @@ path.style = {
 
 path.smooth({ type: 'continuous' });
 
-initializePath();
+view.onMouseMove = function(event){
+    previousMousePoint = mousePoint || CP;
+    mousePoint = event.point;
+    speed = getModule(previousMousePoint, mousePoint);
+};
+
+function wobble (path, event){
+    for(var i = 0; i < path.segments.length; i++){
+        // path.segments[i].point.angle += Math.sin((event.count + i) / 100);
+        path.segments[i].point.length += Math.sin((event.count + i) / 10) * speed/2;
+    }
+}
+
+function getModule(pointA, pointB){
+    var res = Math.sqrt(Math.pow((pointA.x-pointB.x), 2) + Math.pow((pointA.y-pointB.y), 2));
+    return res;
+}
+
+view.onFrame = function(event){
+    // wobble(path, event);
+};
 
 view.onResize = function(){
     // CP.replaceCenter();
-}
+};
+
+initializePath();
 
     
