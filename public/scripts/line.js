ca19dee23b76a9c062a1441fcf48815ef26fe875
damifa <damifa@debian.dami>
2018-04-13 16:07:18 +0200

    add interpolate, pathTarget, pathInit

--- public/scripts/line.js
+++ public/scripts/line.js
@@ -1,4 +1,6 @@
 var path = new Path();
+var pathIni = new Path();
+var pathTarget = new Path();
 var pointCount = 100;
 var step = view.viewSize.width / pointCount;
 var previousMousePoint;
@@ -7,15 +9,20 @@ var speed = 1;
 var output = document.querySelector("#output");
 var input1 = document.querySelector("#input1");
 var input2 = document.querySelector("#input2");
-var pointsIni = [];
 
 var CP = {
     x: 0,
     y: view.center.y,
 };
 
+var values = {
+    force: 0.6
+}
+
 function initializePath(){
     path.segments = [];
+    pathIni.segments = [];
+    pathTarget.segments = [];
 
     for(var i = 1; i < pointCount; i++){
         var tP = new paper.Point({
@@ -24,30 +31,57 @@ function initializePath(){
         });
         
         path.add(tP);
-        pointsIni.push({
-            x: tP.x,
-            y: tP.y
-        });
+
+        pathIni.add(tP)
+        pathTarget.add(tP);
     }
 
     path.style = {
         strokeColor: 'red',
         strokeWidth: 5
     };
+    pathIni.style = {
+        strokeColor: 'green',
+        strokeWidth: 2
+    };
+    pathTarget.style = {
+        strokeColor: 'orange',
+        strokeWidth: 2
+    };
 
     path.smooth({ type: 'continuous' });
     // path.fullySelected = true;
 }
 
+
+function interpolate (){
+    for(var i = 0; i < path.segments.length; i++){
+        path.segments[i].point.y += getDeltaPos(pathTarget.segments[i].point, path.segments[i].point).y * values.force/10; 
+    }
+}
+
 function wobble (path, event, speed){
     for(var i = 0; i < path.segments.length; i++){
-        path.segments[i].point.y = pointsIni[i].y + Math.sin((event.count + i) / input1.value) * input2.value;
+        pathTarget.segments[i].point.y = pathIni.segments[i].point.y + Math.sin((event.count + i) / input1.value) * input2.value;
     }
+}
+
+function getDeltaPos (pointA, pointB){
+    var pos = {};
+    pos.x = pointA.x - pointB.x;
+    pos.y = pointA.y - pointB.y;
+    return pos;
 }
 
 function getDistance(pointA, pointB){
     var res = Math.sqrt(Math.pow((pointA.x-pointB.x), 2) + Math.pow((pointA.y-pointB.y), 2));
     return res;
+}
+
+function setPos(path){
+    for(var i = 0; i < path.segments.length; i++){
+        pathTarget.segments[i].point.y = (Math.random()-0.5)*200 + CP.y;
+    }
 }
 
 view.onMouseMove = function(event){
@@ -57,10 +91,10 @@ view.onMouseMove = function(event){
 };
 
 view.onFrame = function(event){
-    wobble(path, event, speed);
+    // wobble(path, event, speed);
+    interpolate();
     output.innerHTML ="input1: " + input1.value + ", input2: " + input2.value + ", speed: " + speed;
+    if(Key.isDown("space")) setPos(pathTarget);
 }
-
-console.log("brou");
 
 initializePath();
